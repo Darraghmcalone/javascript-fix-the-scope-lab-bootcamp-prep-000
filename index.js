ab232f3cfd5d77cf465c827664a54a9b54e9fa58
darraghmcalone@hotmail.co.uk <darraghmcalone@hotmail.co.uk>
2017-10-26 13:53:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,6 @@ function add2(n) {
   return n + two
 }
 
-  // Feel free to move things around!
-  const two = 2
-    return parseInt(n,10) + two
-}
-
 var funkyFunction = function() {
   return function() {
     "FUNKY!"
