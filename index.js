82997b070ecaa6b6fcc9cf5b70acbc4681257d19
darraghmcalone@hotmail.co.uk <darraghmcalone@hotmail.co.uk>
2017-10-26 13:27:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 
+var animal = 'dog'
+
+function myAnimal(){
+myAnimal ='dog'
+  return myAnimal
+}
+
+function yourAnimal() {
+var animal = 'cat'
+  return animal
+}
+
+function add2(n) {
+  return n + two
+
+  // Feel free to move things around!
+  const two = 2
+    return parseInt(n,10) + two
+}
+
+var funkyFunction = function() {
+  return function() {
+    "FUNKY!"
+  }
+}
+
+// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
+// NOTE: you only need to modify the code below this line.
+theFunk = "FUNKY!"
